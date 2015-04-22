f66ee4bfe1bbf6ef607e33ce2662726ee093ef91
radozaj <radozaj@centrum.sk>
2015-04-22 22:37:54 +0200

    Start new maven project

---
+++ storm/src/main/java/cz/muni/fi/storm/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
