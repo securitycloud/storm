a76773908f9d09a8a00ea67e54b527cdfb7e17fa
radozaj <radozaj@centrum.sk>
2015-05-29 23:54:12 +0200

    Simple topology kafka to kafka is done

---
+++ storm/src/main/java/cz/muni/fi/storm/TopologyKafkaToKafka.java
@@ -0,0 +1,68 @@
+package cz.muni.fi.storm;
+
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import cz.muni.fi.storm.bolts.KafkaProducerBolt;
+import java.util.logging.Logger;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+
+public class TopologyKafkaToKafka {
+
+    private static final Logger log = Logger.getLogger(TopologyKafkaToKafka.class.getName());
+
+    public static void main(String[] args) {
+        log.fine("Starting: Topology-kafka-to-kafka");
+        
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Missing argument: kafka_producer_ip kafka_consumer_ip");
+        }
+
+        String kafkaProducerIp = args[0];
+        String kafkaConsumerIp = args[1];
+        
+        String kafkaProducerPort = "2181";
+        String kafkaConsumerPort = "9092";
+        
+        String kafkaProducerTopic = "securitycloud-testing-data";
+        String kafkaConsumerTopic = "securitycloud-testing-data";
+
+        if (kafkaProducerTopic.equals(kafkaConsumerTopic)
+                && kafkaProducerIp.equals(kafkaConsumerIp)) {
+            throw new IllegalArgumentException("It creates loop! Please differnet kafkas or topics.");
+        }
+
+        String zookeeperHost = kafkaProducerIp + ":" + kafkaProducerPort;
+        ZkHosts zkHosts = new ZkHosts(zookeeperHost);
+        SpoutConfig kafkaConfig = new SpoutConfig(zkHosts, kafkaProducerTopic, "", "storm");
+        kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme() {
+            @Override
+            public Fields getOutputFields() {
+                return new Fields("flow");
+            }
+        });
+
+        KafkaSpout kafkaSpout = new KafkaSpout(kafkaConfig);
+        KafkaProducerBolt kafkaProducerBolt = new KafkaProducerBolt(kafkaConsumerIp, kafkaConsumerPort, kafkaConsumerTopic);
+        
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("kafka-consumer-spout", kafkaSpout);
+        builder.setBolt("kafka-producer-bolt", kafkaProducerBolt)
+                .fieldsGrouping("kafka-consumer-spout", new Fields("flow"));
+
+        Config config = new Config();
+        config.put(Config.TOPOLOGY_TICK_TUPLE_FREQ_SECS, 2);
+
+        try {
+            StormSubmitter.submitTopology("Topology-kafka-to-kafka", config, builder.createTopology());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Couldn't initialize the topology", e);
+        }
+    }
+}
--- storm/src/main/java/cz/muni/fi/storm/bolts/KafkaProducerBolt.java
+++ storm/src/main/java/cz/muni/fi/storm/bolts/KafkaProducerBolt.java
@@ -13,44 +13,45 @@ import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
 public class KafkaProducerBolt extends BaseBasicBolt {
-    private static final Logger log = Logger.getLogger( KafkaProducerBolt.class.getName() );
 
+    private static final Logger log = Logger.getLogger(KafkaProducerBolt.class.getName());
     private Producer<String, String> producer;
     private String kafkaConsumerIp;
-    
-    public KafkaProducerBolt(String kafkaConsumerIp){
-        log.fine("Initializing kafka Producer bolt with kafkaConsumer Ip from constructor");
-        this.kafkaConsumerIp=kafkaConsumerIp;
-    
+    private String kafkaConsumerPort;
+    private String kafkaConsumerTopic;
+
+    public KafkaProducerBolt(String kafkaConsumerIp, String kafkaConsumerPort, String kafkaConsumerTopic) {
+        log.fine("Initializing kafka Producer bolt with kafkaConsumer ip, port and topic from constructor");
+        this.kafkaConsumerIp = kafkaConsumerIp;
+        this.kafkaConsumerPort = kafkaConsumerPort;
+        this.kafkaConsumerTopic = kafkaConsumerTopic;
     }
+
+    @Override
     public void prepare(Map stormConf, TopologyContext context) {
-        log.fine("Prepare consumer KPB");
+        log.fine("Prepare consumer kafka producer bolt");
         Properties props = new Properties();
-        props.put("zookeeper.connect","10.16.31.201:2181");
-        props.put("metadata.broker.list", kafkaConsumerIp+":9092");
-        props.put("broker.id","0");
+        props.put("zookeeper.connect", kafkaConsumerIp + ":2181");
+        props.put("metadata.broker.list", kafkaConsumerIp + ":" + kafkaConsumerPort);
+        props.put("broker.id", "0");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
         props.put("request.required.acks", "1");
         ProducerConfig config = new ProducerConfig(props);
         producer = new Producer<String, String>(config);
-        //fileOut = new SimpleFileOutput((String)stormConf.get("outputFile"));
     }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {}
 
     @Override
-    public void execute (Tuple tuple, BasicOutputCollector collector) {
-        
-        log.fine("Executing and sending data to new topic");
-        // This topic must be different kafka consumer topic.
-        // Because run kafka consumer and producer on localhost, to be loop.
-        KeyedMessage<String, String> data = new KeyedMessage<String, String>("new-topic", tuple.toString());
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        log.fine("Executing and sending data to topic");
+        KeyedMessage<String, String> data = new KeyedMessage<String, String>(kafkaConsumerTopic, tuple.toString());
         producer.send(data);
     }
-    
+
     @Override
-    public void cleanup(){
+    public void cleanup() {
         producer.close();
     }
 }
