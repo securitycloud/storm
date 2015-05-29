b38c197bee7a1c0490a5859f14afa679d95351fd
radozaj <radozaj@centrum.sk>
2015-05-30 00:04:51 +0200

    Simple printer bolt

--- storm/src/main/java/cz/muni/fi/storm/bolts/PrinterBolt.java
+++ storm/src/main/java/cz/muni/fi/storm/bolts/PrinterBolt.java
@@ -7,26 +7,31 @@ import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
 import cz.muni.fi.storm.FileOutput;
 import cz.muni.fi.storm.SimpleFileOutput;
-import cz.muni.fi.storm.TopologyMain;
 import java.util.Map;
 import java.util.logging.Logger;
 
-public class Printer extends BaseBasicBolt {
-     private static final Logger log = Logger.getLogger( Printer.class.getName() );
+public class PrinterBolt extends BaseBasicBolt {
 
+    private static final Logger log = Logger.getLogger(PrinterBolt.class.getName());
+    private String fileName;
     private FileOutput fileOut = null;
 
+    public void Printer(String fileName) {
+        this.fileName = fileName;
+    }
+    
     @Override
     public void prepare(Map stormConf, TopologyContext context) {
         log.fine("Preparing output File - prepare method Printer");
-        fileOut = new SimpleFileOutput((String)stormConf.get("outputFile"));
+        fileOut = new SimpleFileOutput(fileName);
     }
 
     @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {}
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    }
 
     @Override
-    public void execute (Tuple tuple, BasicOutputCollector collector) {
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
         fileOut.append(tuple.toString());
     }
 }
