5109a0561cca18a81430322594e3a1e5680a84ca
radozaj <radozaj@centrum.sk>
2015-05-31 14:53:26 +0200

    deploy-to-cluster.sh: optimized for slow connection

--- storm/scripts/deploy-to-cluster.sh
+++ storm/scripts/deploy-to-cluster.sh
@@ -2,17 +2,22 @@
 
 . scripts/setenv.sh
 
-# COMPILE
-mvn clean package
-if [ "$?" -gt 0 ]
-then
-    exit 1;
-fi
-
 # COPY
-scp target/storm-1.0-SNAPSHOT-jar-with-dependencies.jar root@$SRV_NIMBUS:/$WRK
+tar -cf project.tar src pom.xml
+scp project.tar root@$SRV_NIMBUS:/$WRK
+rm project.tar
 
-# RUN
+# COMPILE & RUN
 ssh root@$SRV_NIMBUS "
-    $WRK/storm/bin/storm jar $WRK/storm-1.0-SNAPSHOT-jar-with-dependencies.jar $@
+    cd $WRK
+    rm -rf project
+    mkdir project
+    tar -xf project.tar -C project
+    cd project
+    mvn clean package
+    if [ "$?" -gt 0 ]
+    then
+        exit 1;
+    fi
+    $WRK/storm/bin/storm jar $WRK/project/target/storm-1.0-SNAPSHOT-jar-with-dependencies.jar $@
 "
