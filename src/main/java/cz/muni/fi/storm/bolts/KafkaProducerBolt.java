7597217345bf1fc4155786c22929d2f95119db21
radozaj <radozaj@centrum.sk>
2015-06-23 20:22:00 +0200

    KafkaProducerBolt: turn off confirming ack

--- src/main/java/cz/muni/fi/storm/bolts/KafkaProducerBolt.java
+++ src/main/java/cz/muni/fi/storm/bolts/KafkaProducerBolt.java
@@ -1,9 +1,9 @@
 package cz.muni.fi.storm.bolts;
 
+import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
 import cz.muni.fi.storm.tools.TupleUtils;
 import java.util.Map;
@@ -13,23 +13,25 @@ import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
-public class KafkaProducerBolt extends BaseBasicBolt {
+public class KafkaProducerBolt extends BaseRichBolt {
 
     private static final Logger log = Logger.getLogger(KafkaProducerBolt.class.getName());
     private Producer<String, String> producer;
     private String kafkaConsumerIp;
     private String kafkaConsumerPort;
     private String kafkaConsumerTopic;
+    private OutputCollector collector;
 
     public KafkaProducerBolt(String kafkaConsumerIp, String kafkaConsumerPort, String kafkaConsumerTopic) {
         log.fine("Initializing kafka Producer bolt with kafkaConsumer ip, port and topic from constructor");
         this.kafkaConsumerIp = kafkaConsumerIp;
         this.kafkaConsumerPort = kafkaConsumerPort;
         this.kafkaConsumerTopic = kafkaConsumerTopic;
+        this.collector = collector;
     }
 
     @Override
-    public void prepare(Map stormConf, TopologyContext context) {
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
         log.fine("Prepare consumer kafka producer bolt");
         Properties props = new Properties();
         props.put("zookeeper.connect", kafkaConsumerIp + ":2181");
@@ -46,7 +48,7 @@ public class KafkaProducerBolt extends BaseBasicBolt {
     public void declareOutputFields(OutputFieldsDeclarer declarer) {}
 
     @Override
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
+    public void execute(Tuple tuple) {
         if (TupleUtils.isTickTuple(tuple) == false) {
             log.fine("Executing and sending data to topic");
             KeyedMessage<String, String> data = new KeyedMessage<String, String>(kafkaConsumerTopic, tuple.getValue(0).toString());
