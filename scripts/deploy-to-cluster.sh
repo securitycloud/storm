93abee0b5c3169b714825d9a7b04878f61a64f9b
radozaj <radozaj@centrum.sk>
2015-06-27 17:52:05 +0200

    deploy-to-cluster: no compile if it was compiled

--- scripts/deploy-to-cluster.sh
+++ scripts/deploy-to-cluster.sh
@@ -4,20 +4,25 @@
 
 # COPY
 tar -cf project.tar src pom.xml
-scp project.tar root@$SRV_NIMBUS:/$WRK
+scp project.tar root@$SRV_NIMBUS:/$WRK/project.new.tar
 rm project.tar
 
 # COMPILE & RUN
 ssh root@$SRV_NIMBUS "
     cd $WRK
-    rm -rf project
-    mkdir project
-    tar -xf project.tar -C project
-    cd project
-    mvn clean package
+    diff project.tar project.new.tar > /dev/nul
     if [ "$?" -gt 0 ]
     then
-        exit 1;
+        rm -rf project
+        mkdir project
+        mv project.new.tar project.tar
+        tar -xf project.tar -C project
+        cd project
+        mvn clean package
+        if [ "$?" -gt 0 ]
+        then
+            exit 1;
+        fi
     fi
     $WRK/storm/bin/storm jar $WRK/project/target/storm-1.0-SNAPSHOT-jar-with-dependencies.jar $@ $KAFKA_PRODUCER $KAFKA_CONSUMER
 "
