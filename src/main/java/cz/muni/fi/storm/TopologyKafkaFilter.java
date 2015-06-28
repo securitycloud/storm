9b62dc7781f10f917fa940c62646de56eaf39dcf
radozaj <radozaj@centrum.sk>
2015-06-28 13:01:18 +0200

    TopologyKafkaFilter: (minor) correct label

--- src/main/java/cz/muni/fi/storm/TopologyKafkaFilter.java
+++ src/main/java/cz/muni/fi/storm/TopologyKafkaFilter.java
@@ -58,7 +58,7 @@ public class TopologyKafkaFilter {
         config.setDebug(false);
 
         try {
-            StormSubmitter.submitTopology("Topology-kafka-filter-kafka", config, builder.createTopology());
+            StormSubmitter.submitTopology("Topology-kafka-filter", config, builder.createTopology());
         } catch (Exception e) {
             e.printStackTrace();
             throw new IllegalStateException("Couldn't initialize the topology", e);
