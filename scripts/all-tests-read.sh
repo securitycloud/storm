64618ed9d3f98698ef50d07db4ca97abdc1ad97c
radozaj <radozaj@centrum.sk>
2015-08-06 21:30:45 +0200

    all-tests-read.sh: loop for each topology

--- scripts/all-tests-read.sh
+++ scripts/all-tests-read.sh
@@ -12,8 +12,11 @@ COMPUTERS[1]=1
 COMPUTERS[2]=3
 COMPUTERS[3]=5
 
+REPEAT=5
+
 NUM_TESTS=${#TOPOLOGIES[@]}
 NUM_TESTS=$((NUM_TESTS * ${#COMPUTERS[@]}))
+NUM_TESTS=$((NUM_TESTS * ${REPEAT}))
 ACT_TEST=1
 
 $CUR_DIR/clean/clean-cluster.sh
@@ -21,17 +24,20 @@ $CUR_DIR/install/install-cluster.sh
 $CUR_DIR/start/start-cluster.sh
 $CUR_DIR/run/recreate-topic.sh $SERVICE_TOPIC 1 $KAFKA_CONSUMER
 
-for TP in "${TOPOLOGIES[@]}"
+for PC in "${COMPUTERS[@]}"
 do
-    for PC in "${COMPUTERS[@]}"
-    do
-        $CUR_DIR/run/log-to-service-topic.sh "Input topic for read tests: Partitions=$PC"
-        $CUR_DIR/run/recreate-topic.sh $TESTING_TOPIC $PC $KAFKA_PRODUCER
-        $CUR_DIR/run/run-input.sh 5000
+    $CUR_DIR/run/log-to-service-topic.sh "Input topic for read tests: Partitions=$PC"
+    $CUR_DIR/run/recreate-topic.sh $TESTING_TOPIC $PC $KAFKA_PRODUCER
+    $CUR_DIR/run/run-input.sh 5000
 
-        echo -e $LOG Running test $ACT_TEST/$NUM_TESTS: $OFF
-        $CUR_DIR/run/run-test-read.sh $TP $PC
-        ACT_TEST=$((ACT_TEST + 1))
+    for i in `seq 1 $REPEAT`
+    do
+        for TP in "${TOPOLOGIES[@]}"
+        do
+            echo -e $LOG Running test $ACT_TEST/$NUM_TESTS: $OFF
+            $CUR_DIR/run/run-test-read.sh $TP $PC
+            ACT_TEST=$((ACT_TEST + 1))
+        done
     done
 done
 
