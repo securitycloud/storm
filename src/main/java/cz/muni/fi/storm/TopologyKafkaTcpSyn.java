1562d99409daf469f8de03133236d44903cab4a6
severinsimko@gmail.com <severinsimko@gmail.com>
2015-08-12 04:44:12 +0200

    TCP SYN task updated,

---
+++ src/main/java/cz/muni/fi/storm/TopologyKafkaTcpSyn.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.storm;
+
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.IRichSpout;
+import backtype.storm.topology.TopologyBuilder;
+import cz.muni.fi.storm.bolts.GlobalCountWindowBolt;
+import cz.muni.fi.storm.bolts.TcpSynGlobalSortKafkaBolt;
+import cz.muni.fi.storm.bolts.TcpSynPacketCounterBolt;
+import cz.muni.fi.storm.spouts.KafkaSpout;
+import cz.muni.fi.storm.tools.ServiceCounter;
+import cz.muni.fi.storm.tools.TopologyUtil;
+import cz.muni.fi.storm.tools.TupleUtils;
+import java.util.logging.Logger;
+
+public class TopologyKafkaTcpSyn{
+
+    private static final Logger log = Logger.getLogger(TopologyKafkaTopNKafka.class.getName());
+
+    public static void main(String[] args) {
+        log.fine("Starting: TopologyKafkaTcpSyn");
+        
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Missing argument: number_of_computers from_beginning");
+        }
+        
+        int numberOfComputers = Integer.parseInt(args[0]);        
+        boolean fromBeginning = ("true".equals(args[1])) ? true : false;
+
+        IRichSpout kafkaSpout = new KafkaSpout(fromBeginning, true);
+        IRichBolt tcpSynPacketCounterBolt = new TcpSynPacketCounterBolt();
+        IRichBolt tcpSynGlobalSortKafkaBolt = new TcpSynGlobalSortKafkaBolt(numberOfComputers);
+        IRichBolt globalCountWindowBolt = new GlobalCountWindowBolt();
+        
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("kafkaSpout", kafkaSpout, numberOfComputers);
+        builder.setBolt("tcpSynPacketCounterBolt", tcpSynPacketCounterBolt, numberOfComputers)
+                .localOrShuffleGrouping("kafkaSpout")
+                .localOrShuffleGrouping("kafkaSpout", TupleUtils.getStreamIdForEndOfWindow());
+        builder.setBolt("globalSortKafkaBolt", tcpSynGlobalSortKafkaBolt)
+                .globalGrouping("tcpSynPacketCounterBolt")
+                .globalGrouping("tcpSynPacketCounterBolt", TupleUtils.getStreamIdForEndOfWindow());
+        builder.setBolt("globalCountWindowBolt", globalCountWindowBolt)
+                .globalGrouping("tcpSynPacketCounterBolt", ServiceCounter.getStreamIdForService());
+
+        Config config = new Config();
+        config.setNumWorkers(numberOfComputers);
+        config.putAll(new TopologyUtil().loadProperties());
+
+        try {
+            StormSubmitter.submitTopology("TopologyKafkaTcpSyn", config, builder.createTopology());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Couldn't initialize the topology", e);
+        }
+    }
+}
---
+++ src/main/java/cz/muni/fi/storm/bolts/TcpSynGlobalSortKafkaBolt.java
@@ -0,0 +1,93 @@
+package cz.muni.fi.storm.bolts;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.muni.fi.storm.tools.TupleUtils;
+import cz.muni.fi.storm.tools.ValueComparator;
+import cz.muni.fi.storm.tools.pojo.PacketCount;
+import cz.muni.fi.storm.tools.writers.KafkaProducer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TcpSynGlobalSortKafkaBolt extends BaseRichBolt {
+
+    private ObjectMapper mapper;
+    private int topN;
+    private int totalSenders;
+    private int actualSenders;
+    private HashMap<String, Long> totalCounter;
+    private KafkaProducer kafkaProducer;
+
+
+    public TcpSynGlobalSortKafkaBolt(int totalSenders) {
+        this.totalSenders = totalSenders;
+    }
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.mapper = new ObjectMapper();
+        this.totalCounter = new HashMap<String, Long>();
+        this.actualSenders = 0;
+        this.topN = new Integer(stormConf.get("sort.topN").toString());
+        
+        String broker = (String) stormConf.get("kafkaProducer.broker");
+        int port = new Integer(stormConf.get("kafkaProducer.port").toString());
+        String topic = (String) stormConf.get("kafkaProducer.topic");
+        this.kafkaProducer = new KafkaProducer(broker, port, topic);
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        if (TupleUtils.isEndOfWindow(tuple)) {
+            actualSenders++ ;
+            if (actualSenders == totalSenders) {
+                ValueComparator valueComparator =  new ValueComparator(totalCounter);
+                TreeMap<String, Long> sortedTotalCounter = new TreeMap<String, Long>(valueComparator);
+                
+                //vyberie z mapy(totalCounter) iba tie ktore maju Value viac ako 10
+                for (Map.Entry<String, Long> entry : totalCounter.entrySet()) {
+                     if(entry.getValue()>10){
+                         sortedTotalCounter.put(entry.getKey(),entry.getValue());                     
+                           }    
+                        }
+
+                
+                for (String ip : sortedTotalCounter.keySet()) {
+                    PacketCount packetCount = new PacketCount();
+                    packetCount.setDestIpAddr(ip);
+                    packetCount.setPackets(totalCounter.get(ip));
+                    try {
+                        String packetCountJson = mapper.writeValueAsString(packetCount);
+                        kafkaProducer.send(packetCountJson);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException("Can not create JSON from PacketCount", e);
+                    }
+                    break;
+                }
+            }
+
+        } else {
+            String ip = tuple.getString(0);
+            long packets = tuple.getLong(1);
+            
+            if (totalCounter.containsKey(ip)) {
+                packets += totalCounter.get(ip);
+            }
+            totalCounter.put(ip, packets);
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {}
+
+    @Override
+    public void cleanup() {
+        kafkaProducer.close();
+    }
+}
