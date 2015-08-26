0f60cfc21543699fed1b79baaeeacf9f42808f86
Radoslav Zajonc <radozaj@centrum.sk>
2015-08-26 21:47:15 +0200

    TopologyEmpty: easy test KafkaSpout

--- src/main/java/cz/muni/fi/storm/TopologyEmpty.java
+++ src/main/java/cz/muni/fi/storm/TopologyEmpty.java
@@ -2,14 +2,22 @@ package cz.muni.fi.storm;
 
 import backtype.storm.Config;
 import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import cz.muni.fi.storm.bolts.FilterCounterBolt;
 import cz.muni.fi.storm.bolts.GlobalCountWindowBolt;
 import cz.muni.fi.storm.bolts.GlobalCounterBolt;
+import cz.muni.fi.storm.bolts.KafkaBolt;
 import cz.muni.fi.storm.spouts.KafkaCounterSpout;
 import cz.muni.fi.storm.tools.ServiceCounter;
 import cz.muni.fi.storm.tools.TopologyUtil;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
 
 public class TopologyEmpty {
 
@@ -23,8 +31,27 @@ public class TopologyEmpty {
         Config config = new Config();
         config.setNumWorkers(numberOfComputers);
         config.putAll(new TopologyUtil().loadProperties());
+        
+        
+        ZkHosts zkHosts = new ZkHosts("100.64.25.107:2181");
+        SpoutConfig kafkaConfig = new SpoutConfig(zkHosts, "dataset-5part", "", "storm");
+        kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme() {
+                @Override
+                public Fields getOutputFields() {
+                    return new Fields("flow");
+                }
+            });
+        kafkaConfig.startOffsetTime = kafka.api.OffsetRequest.EarliestTime();
+        
+        IRichSpout kafkaSpout = new KafkaSpout(kafkaConfig);
+        IRichBolt kafkaBolt = new KafkaBolt();
+        
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("kafkaSpout", kafkaSpout, numberOfComputers);
+        builder.setBolt("kafkaBolt", kafkaBolt, numberOfComputers)
+                .localOrShuffleGrouping("kafkaSpout");
 
-        IRichSpout kafkaCounterSpout = new KafkaCounterSpout();
+        /*IRichSpout kafkaCounterSpout = new KafkaCounterSpout();
         IRichBolt globalCounterBolt = new GlobalCounterBolt(numberOfComputers);
         IRichBolt globalCountWindowBolt = new GlobalCountWindowBolt(numberOfComputers);
         
@@ -34,7 +61,7 @@ public class TopologyEmpty {
                 .globalGrouping("kafkaCounterSpout");
         builder.setBolt("globalCountWindowBolt", globalCountWindowBolt)
                 .globalGrouping("kafkaCounterSpout", ServiceCounter.getStreamIdForService());
-        
+        */
         try {
             StormSubmitter.submitTopology("TopologyEmpty", config, builder.createTopology());
         } catch (Exception e) {
---
+++ src/main/java/cz/muni/fi/storm/bolts/KafkaBolt.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.storm.bolts;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+import cz.muni.fi.storm.tools.writers.KafkaProducer;
+import java.util.Map;
+
+public class KafkaBolt extends BaseRichBolt {
+
+    private KafkaProducer kafkaProducer;
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        String broker = (String) stormConf.get("kafkaProducer.broker");
+        int port = new Integer(stormConf.get("kafkaProducer.port").toString());
+        String topic = (String) stormConf.get("kafkaProducer.topic");
+        int totalTasks = context.getComponentTasks(context.getThisComponentId()).size();
+        
+        this.kafkaProducer = new KafkaProducer(broker, port, topic);
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        kafkaProducer.send(tuple.getValue(0).toString());
+    }
+    
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) { }
+
+    @Override
+    public void cleanup() {
+        kafkaProducer.close();
+    }
+}
