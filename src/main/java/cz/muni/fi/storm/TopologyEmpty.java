b84f9e53bf1f150ab69f1a04813ac129f04d94c8
Radoslav Zajonc <radozaj@centrum.sk>
2015-08-27 10:34:29 +0200

    TopologyEmpty: fixed

--- src/main/java/cz/muni/fi/storm/TopologyEmpty.java
+++ src/main/java/cz/muni/fi/storm/TopologyEmpty.java
@@ -7,14 +7,11 @@ import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
-import cz.muni.fi.storm.bolts.FilterCounterBolt;
+import cz.muni.fi.storm.bolts.CounterBolt;
 import cz.muni.fi.storm.bolts.GlobalCountWindowBolt;
-import cz.muni.fi.storm.bolts.GlobalCounterBolt;
-import cz.muni.fi.storm.bolts.KafkaBolt;
-import cz.muni.fi.storm.spouts.KafkaCounterSpout;
-import cz.muni.fi.storm.spouts.MyKafkaSpout;
 import cz.muni.fi.storm.tools.ServiceCounter;
 import cz.muni.fi.storm.tools.TopologyUtil;
+import storm.kafka.KafkaSpout;
 import storm.kafka.SpoutConfig;
 import storm.kafka.StringScheme;
 import storm.kafka.ZkHosts;
@@ -32,41 +29,27 @@ public class TopologyEmpty {
         config.setNumWorkers(numberOfComputers);
         config.putAll(new TopologyUtil().loadProperties());
         
-        /*
         ZkHosts zkHosts = new ZkHosts("100.64.25.107:2181");
-        SpoutConfig kafkaConfig = new SpoutConfig(zkHosts, "dataset-5part", "", "storm");
+        SpoutConfig kafkaConfig = new SpoutConfig(zkHosts, "dataset-" + numberOfComputers + "part", "", "storm");
         kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme() {
                 @Override
                 public Fields getOutputFields() {
                     return new Fields("flow");
                 }
             });
-        kafkaConfig.startOffsetTime = kafka.api.OffsetRequest.EarliestTime();
         kafkaConfig.forceFromStart = true;
         
-        IRichSpout kafkaSpout = new KafkaSpout(kafkaConfig);*/
-        IRichSpout kafkaSpout = new MyKafkaSpout();
-        IRichBolt kafkaBolt = new KafkaBolt();
+        IRichSpout kafkaSpout = new KafkaSpout(kafkaConfig);
+        IRichBolt counterBolt = new CounterBolt();
         IRichBolt globalCountWindowBolt = new GlobalCountWindowBolt(numberOfComputers);
         
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("kafkaSpout", kafkaSpout, numberOfComputers);
-        builder.setBolt("kafkaBolt", kafkaBolt, numberOfComputers)
+        builder.setBolt("counterBolt", counterBolt, numberOfComputers)
                 .localOrShuffleGrouping("kafkaSpout");
         builder.setBolt("globalCountWindowBolt", globalCountWindowBolt)
-                .globalGrouping("kafkaBolt", ServiceCounter.getStreamIdForService());
+                .globalGrouping("counterBolt", ServiceCounter.getStreamIdForService());
 
-        /*IRichSpout kafkaCounterSpout = new KafkaCounterSpout();
-        IRichBolt globalCounterBolt = new GlobalCounterBolt(numberOfComputers);
-        IRichBolt globalCountWindowBolt = new GlobalCountWindowBolt(numberOfComputers);
-        
-        TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("kafkaCounterSpout", kafkaCounterSpout, numberOfComputers);
-        builder.setBolt("globalCounterBolt", globalCounterBolt)
-                .globalGrouping("kafkaCounterSpout");
-        builder.setBolt("globalCountWindowBolt", globalCountWindowBolt)
-                .globalGrouping("kafkaCounterSpout", ServiceCounter.getStreamIdForService());
-        */
         try {
             StormSubmitter.submitTopology("TopologyEmpty", config, builder.createTopology());
         } catch (Exception e) {
