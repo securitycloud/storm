bc84fca7520a044f4488700e651429bd9a48debe
Radoslav Zajonc <radozaj@centrum.sk>
2015-08-31 17:45:54 +0200

    result-parse.sh: new requirements

--- scripts/run/end/result-parse.sh
+++ scripts/run/end/result-parse.sh
@@ -17,30 +17,33 @@ declare -a TEST_MAX=( $(for i in {1..100}; do echo 0; done) )
 
 while read LINE
 do
-    # LINE IS NAME OF TEST
-    if [[ ${LINE::1} =~ "T" ]]
+    # NAME OF TEST
+    LABEL=`echo $LINE | sed 's/^ Running topology \(.*\)$/\1/'`
+    if [[ $LABEL =~ $Topology.* ]]
     then
         i=1
         for NAME in "${TEST_NAME[@]}"
         do
-            if [ "$NAME" = "$LINE" ] 
+            if [ "$NAME" = "$LABEL" ] 
             then
                 break
             fi
             i=$((i + 1))
         done
-        TEST_NAME[i]="$LINE"
+        TEST_NAME[i]="$LABEL"
         TEST_INDEX=$i
     fi
     
-    # FIRST WORD OF LINE IS NUMBER = RESULT OF TEST
-    RESULT=`echo $LINE | sed 's/^\([0-9]*\).*$/\1/'`
-    if [ -n "$RESULT" ]
+    # RESULT OF TEST
+    RESULT=`echo $LINE | sed 's/^This run took: \([0-9]*\) milli_seconds$/\1/'`
+    if [[ $RESULT =~ ^[0-9]+$ ]]
     then
+        (( RESULT = 1000000 / RESULT ))
         (( TEST_COUNT[TEST_INDEX]++ ))
         (( TEST_SUM[TEST_INDEX] += RESULT ))
         (( TEST_MAX[TEST_INDEX] = RESULT > TEST_MAX[TEST_INDEX] ? RESULT : TEST_MAX[TEST_INDEX] ))
         (( TEST_MIN[TEST_INDEX] = RESULT < TEST_MIN[TEST_INDEX] ? RESULT : TEST_MIN[TEST_INDEX] ))
+        TEST_ALL[$TEST_INDEX]=${TEST_ALL[$TEST_INDEX]}, $RESULT
     fi    
 
 done < $SOURCE
@@ -53,6 +56,7 @@ do
         echo ${TEST_NAME[$i]}
         (( AVG = TEST_SUM[i] / TEST_COUNT[i] ))
         echo "Results for ${TEST_COUNT[$i]} tests:"
+        echo "  all = ${TEST_ALL[$i]}"
         echo "  min = ${TEST_MIN[$i]} flows / s"
         echo "  avg = $AVG flows / s"
         echo "  max = ${TEST_MAX[$i]} flows / s"
